24101d5e7017a1f44783918e32a969619dd1dc94
Harrshaa <sriharsha.mutakodoor@gmail.com>
2023-10-08 11:07:19 +0530

    verified

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import verifyIcon from '../utils/verifyIcon.svg'
 
 
 const Chat = ({ item }) => {
@@ -7,7 +8,7 @@ const Chat = ({ item }) => {
             { item?.slice().reverse().map((item,index) => <div key={index} className={`d-flex pt-4 ${(item?.sender?.self) ? 'flex-row-reverse' : ""}`}>
                 <div className={`position-relative me-2 ${(item?.sender?.self) ? 'd-none' : ""}`}>
                     <img className='profile_photo' src={item?.sender?.image}  alt="profile_photo" />
-                    {/* <span className={`verifyIcom ${(item?.sender?.is_kyc_verified) ? 'd-none' : ""}`}><img src={verifyIcon} className='verifyIcon1' alt="verify" /></span> */}
+                    <span className={`verifyIcom ${(item?.sender?.is_kyc_verified) ? 'd-none' : ""}`}><img src={verifyIcon} className='verifyIcon1' alt="verify" /></span>
                 </div>
                 <div className={`${(item?.sender?.self) ? 'message2' : "message"}`}>
                     <p className={`message-font ${(item?.sender?.self) ? 'text-light' : ""}`}>{item?.message} </p>
